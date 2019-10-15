2521f0ff19fa03974f1aa96e63eee508a6889beb
iamsj7 <shaikjaleel270@gmail.com>
2019-10-15 15:28:17 +0530

    ShaikJaleel: WebView: only allow loading client url
    Signed-off-by: iamsj7 <shaikjaleel270@gmail.com>

--- app/src/main/java/com/iamshaikjaleel/SJAPP/MainActivity.java
+++ app/src/main/java/com/iamshaikjaleel/SJAPP/MainActivity.java
@@ -2,6 +2,8 @@ package com.iamshaikjaleel.SJAPP;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.webkit.WebResourceRequest;
@@ -18,9 +20,15 @@ public class MainActivity extends AppCompatActivity {
         myWebView.loadUrl("http://www.shaikjaleel.me");
         myWebView.setWebViewClient(new WebViewClient() {
             @Override
-            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-                view.loadUrl(request.getUrl().toString());
-                return false;
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if ("www.shaikjaleel.me".equals(Uri.parse(url).getHost())) {
+                    // This is my website, so do not override; let my WebView load the page
+                    return false;
+                }
+                // Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+                return true;
             }
         });
     }
